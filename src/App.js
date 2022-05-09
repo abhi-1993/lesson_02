7c1d95b0ee8bc3b5a48357d82328b94fa128e36c
Abhinendra Chauhan <abhinendra.chauhan1993@gmail.com>
2022-05-10 01:17:29 +0530

    First commit to the Posts Application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import PostList from './feature/posts/PostsList';
+import AddPostForm from './feature/posts/AddPostForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <AddPostForm/>
+      <PostList/>
+    </main>
   );
 }
 
